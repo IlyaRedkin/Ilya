30a564a65a87650ef8e2dea9d2dd63c3eb2e0d95
IlyaRedkin <ilyaredkin@gmail.com>
2016-12-16 04:17:17 +0300

    задание 3 - реализация в лоб

--- JS_1/hw_1_5/index_js1_hw_5.js
+++ JS_1/hw_1_5/index_js1_hw_5.js
@@ -39,3 +39,13 @@ function NumToObj(number){
 	digit['\'сотни\''] = a2;
 	return digit;
 }
+
+// * Реализовать функцию objectToQueryString(object), которая принимает аргументом объект,
+// возвращает строку. Примеры работы:
+// console ​.log(objectToQueryString({user: ​'Dmitry' ​})); ​// user=Dmitry
+// console ​.log(objectToQueryString({user: ​'Dmitry' ​, password: ​'pass' ​})); ​// user=Dmitry&password=pass 
+// ​console ​.log(objectToQueryString({user: ​'Dmitry' ​, password: 'pass' ​, id= ​1 ​})); ​// user=Dmitry&password=pass&id=1
+
+function objectToQueryString(object){
+	console.log(object);
+}
